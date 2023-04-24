ca98bc05e97f21d0f89165d3b1238d58fac97b6e
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2023-04-24 16:26:20 -0300

    feat: create rmq module and service

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,3 +1,5 @@
 export * from './database/database.abstract';
 export * from './database/repository.abstract';
 export * from './database/schema.abstract';
+export * from './rmq/rmq.module';
+export * from './rmq/rmq.service';
